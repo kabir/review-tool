3a21643e25cf802e44265757265234c09ac15ad4
Kabir Khan <kkhan@redhat.com>
2019-03-12 17:24:23 +0100

    Initial entities for organisation and mirrored repositories

---
+++ src/main/java/org/overbaard/review/tool/config/github/MirroredRepository.java
@@ -0,0 +1,101 @@
+package org.overbaard.review.tool.config.github;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+/**
+ * @author <a href="mailto:kabir.khan@jboss.com">Kabir Khan</a>
+ */
+@Entity
+@Table(name = "gh_mirrored_repository")
+public class MirroredRepository {
+    @Id
+    @SequenceGenerator(
+            name = "ghMirroredRepositorySequence",
+            sequenceName = "gh-mirrored_repository_id_seq",
+            allocationSize = 1,
+            initialValue = 10)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ghMirroredRepositorySequence")
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "gh_organisation_id")
+    Organisation organisation;
+
+    @Column(name = "upstream_organisation", nullable = false)
+    String upstreamOrganisation;
+    @Column(name = "upstream_repository", nullable = false)
+    String upstreamRepository;
+
+    public MirroredRepository() {
+
+    }
+
+    public MirroredRepository(Organisation organisation, String upstreamOrganisation, String upstreamRepository) {
+        this.organisation = organisation;
+        this.upstreamOrganisation = upstreamOrganisation;
+        this.upstreamRepository = upstreamRepository;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Organisation getOrganisation() {
+        return organisation;
+    }
+
+    public void setOrganisation(Organisation organisation) {
+        this.organisation = organisation;
+    }
+
+    public String getUpstreamOrganisation() {
+        return upstreamOrganisation;
+    }
+
+    public void setUpstreamOrganisation(String upstreamOrganisation) {
+        this.upstreamOrganisation = upstreamOrganisation;
+    }
+
+    public String getUpstreamRepository() {
+        return upstreamRepository;
+    }
+
+    public void setUpstreamRepository(String upstreamRepository) {
+        this.upstreamRepository = upstreamRepository;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MirroredRepository that = (MirroredRepository) o;
+
+        if (!id.equals(that.id)) return false;
+        if (!organisation.equals(that.organisation)) return false;
+        if (!upstreamOrganisation.equals(that.upstreamOrganisation)) return false;
+        return upstreamRepository.equals(that.upstreamRepository);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + organisation.hashCode();
+        result = 31 * result + upstreamOrganisation.hashCode();
+        result = 31 * result + upstreamRepository.hashCode();
+        return result;
+    }
+}
---
+++ src/main/java/org/overbaard/review/tool/config/github/Organisation.java
@@ -0,0 +1,80 @@
+package org.overbaard.review.tool.config.github;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+/**
+ * @author <a href="mailto:kabir.khan@jboss.com">Kabir Khan</a>
+ */
+@Entity
+@Table(name = "gh_organisation")
+public class Organisation {
+
+    @Id
+    @SequenceGenerator(
+            name = "ghOrganisationSequence",
+            sequenceName = "gh-organisation_id_seq",
+            allocationSize = 1,
+            initialValue = 10)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ghOrganisationSequence")
+    private Integer id;
+
+    @Column(name="org_name", unique = true, length = 255, nullable = false) // 255 should be long enough although GH doesn't seem to really have a limit
+    private String orgName;
+
+    @Column(name="tool_repo", nullable = false)
+    private String toolPrRepo;
+
+    @OneToMany
+    List<MirroredRepository> mirroredRepositories;
+
+    public Organisation() {
+    }
+
+    public Organisation(String orgName, String reviewRepo) {
+        this.orgName = orgName;
+        this.toolPrRepo = reviewRepo;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getOrgName() {
+        return orgName;
+    }
+
+    public void setOrgName(String orgName) {
+        this.orgName = orgName;
+    }
+
+    public String getToolPrRepo() {
+        return toolPrRepo;
+    }
+
+    public void setToolPrRepo(String toolPrRepo) {
+        this.toolPrRepo = toolPrRepo;
+    }
+
+    public void addMirroredRepository(MirroredRepository mirroredRepository) {
+        mirroredRepositories.add(mirroredRepository);
+        mirroredRepository.setOrganisation(this);
+    }
+
+    public void removeMirroredRepository(MirroredRepository mirroredRepository) {
+        mirroredRepositories.remove(mirroredRepository);
+        mirroredRepository.setOrganisation(null);
+    }
+}
