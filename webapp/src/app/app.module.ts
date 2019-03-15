4d1a95c23a52a487ccbfe5c6c465504c48b08064
Kabir Khan <kkhan@redhat.com>
2019-03-15 17:09:31 +0100

    Remove temp test modules

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -3,19 +3,13 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {TestAComponent} from './test-a/test-a.component';
-import {TestBComponent} from './test-b/test-b.component';
-import { TestCComponent } from './test-c/test-c.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from './material.module';
 import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TestAComponent,
-    TestBComponent,
-    TestCComponent
+    AppComponent
   ],
   imports: [
     BrowserAnimationsModule,
