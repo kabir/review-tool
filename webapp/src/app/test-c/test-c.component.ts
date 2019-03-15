4d8e8f78f733b722ca5a303112bf0a45a822e574
Kabir Khan <kkhan@redhat.com>
2019-03-15 10:22:04 +0100

    Start trying to bring in external content
    Use dom sanitizer to make angular allow the usage

--- webapp/src/app/test-c/test-c.component.ts
+++ webapp/src/app/test-c/test-c.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-test-c',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestCComponent implements OnInit {
 
-  constructor() { }
+  url: string;
+
+  constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
+  }
+
+  updateUrl(value: any) {
+    console.log(value);
+    this.url = value;
+  }
+
+  onChangeUrl(event: Event, value: string) {
+    console.log(value);
+    let url: any;
+    if (value.trim().length === 0) {
+      url = null;
+    } else {
+      url = this.sanitizer.bypassSecurityTrustResourceUrl(value);
+      console.log(url);
+    }
+    this.url = url;
+
   }
 
 }
